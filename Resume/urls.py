eaf2f53d1d34e9311df2316cf81bd04c1888e9da
Jenin-Joy <jeninjoy21@gmail.com>
2025-04-26 13:11:01 +0530

    DataBase Updation

--- Resume/urls.py
+++ Resume/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Basics/',include("Basics.urls")),
     path('Administrator/',include("Administrator.urls")),
     path('',include("Guest.urls")),
     path('User/',include("User.urls")),
